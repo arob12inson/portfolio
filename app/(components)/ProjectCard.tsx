374d08be67738925851f78742d59571574814946
Aidan Robinson <120530116+arob26inson@users.noreply.github.com>
2024-01-06 00:37:54 -0800

    I'm going to bed.

--- app/(components)/ProjectCard.tsx
+++ app/(components)/ProjectCard.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
-const ProjectCard = () => {
+const ProjectCard = ({ title }: { title: string }) => {
   return (
-    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2 min-w-full text-black">
-      <h4>Title</h4>
+    <div
+      className="flex flex-col bg-card hover:bg-card-hover 
+    rounded-md shadow-lg p-3 m-2 w-full lg:w-5/12 xl:w-1/4 h-1/4 text-black"
+    >
+      <h4>{title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap text-black">
+      <p className="whitespace-pre-wrap text-black oveflow-hidden">
         Lorem ipsum dolor sit amet,
       </p>
       <div className="flex-grow"></div>
@@ -13,6 +16,7 @@ const ProjectCard = () => {
         <div className="ml-auto flex items-end">Status display</div>
       </div>
       {/* TODO maybe add some icons? like the github repo? */}
+      {/* TODO make cards all the same fixed size, no overflow allowed */}
     </div>
 
     // <div className="flex flex-col">
--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -3,15 +3,11 @@ import ProjectCard from "../(components)/ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="p-6 lg:grid grid-cols-2 xl:grid-cols-3 gap-2 justify-items-center items-center">
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
+    <div className="p-6 flex flex-wrap w-full h-full justify-items-start ">
+      <ProjectCard title="AI From Scratch" />
+
+      <ProjectCard title="Portfolio Site!" />
+      <ProjectCard title="Portfolio Site!" />
     </div>
   );
 };
